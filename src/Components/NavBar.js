f476a435f83b46067f8896953fa781ebc0b9bf20
Emily <emilybribragg@gmail.com>
2022-07-27 10:06:42 -0400

    Added code for basic navigation bar with links

---
+++ src/Components/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+function NavBar() {
+  return (
+    <div className="navigation">
+      <Navbar bg="light" variant="light" className="navBar">
+        <div className="navContainer">
+         <div className="title">Don't Sweat It</div>
+         <Nav className="me-auto">
+          <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+          <Nav.Link as={NavLink} to="/exerciselist">Exercises</Nav.Link>
+          <Nav.Link as={NavLink} to="/workoutlist">Workouts</Nav.Link>
+         </Nav>
+        </div>
+      </Navbar>
+    </div>
+  )
+}
+
+export default NavBar;
