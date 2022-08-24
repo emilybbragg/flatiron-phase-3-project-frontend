403c13279dda3f4875bd366fac70e4e99e85bdad
Emily <emilybribragg@gmail.com>
2022-08-24 11:28:47 -0400

    Fixed PATCH request

--- src/Components/Workout.js
+++ src/Components/Workout.js
@@ -1,20 +1,81 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
-function Workout( {workout, handleDeleteWorkout} ) {
+function Workout( {workout, handleDeleteWorkout, exerciseArray} ) {
 
+  const [selectedExercises, setSelectedExercises] = useState([])
+  const [workoutExercises, setWorkoutExercises] = useState([])
+
+    useEffect(() => {
+    }, [selectedExercises])
+
+  const allExercises = exerciseArray.map((exercise) => {
     return (
-        <div className="workoutItemContainer">
-        <li className="workoutItems">
-            <strong>{workout.name}</strong>
-            <br></br>
-            <span>Description: {workout.description}</span>
-            <br></br>
-            <button className="deleteButton gg-trash"
-              onClick={() => handleDeleteWorkout(workout)}>
-            </button>
-        </li>
-        </div>
-      );
+        <>
+            <input type="checkbox" value={exercise.id} defaultChecked={workoutExercises.find(workoutExercise => exercise.id == workoutExercise.id)} name={exercise.name} onChange={(e) => handleSelectExercise(e)}/>
+            <label htmlFor={exercise.name}>{exercise.name}</label>
+        </>
+    )
+  })
+
+  const allWorkoutExercises = workoutExercises.map(exercise => {
+        return (
+            <li className="exerciseWorkoutList" key={exercise.id}>{exercise.name}</li>
+        )
+  })
+
+  function handleAddExercises(e) {    
+      e.preventDefault();
+      const workoutData = {
+        exercises: selectedExercises
+      };
+      fetch(`http://localhost:9292/workouts/${workout.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+          body: JSON.stringify(workoutData),
+      })
+      .then((r) => r.json())
+      .then((exercisesData) => {
+          setWorkoutExercises(exercisesData)
+      })
   }
+
+  function handleSelectExercise(e) {
+      const exercises = [...selectedExercises]
+
+      if (exercises.includes(e.target.value)) {
+          const exerciseIndex = exercises.indexOf(e.target.value)
+          exercises.splice(exerciseIndex, 1)
+      } else {
+          exercises.push(e.target.value)
+      }
+      setSelectedExercises(exercises)
+  }
+
+  return (
+    <div className="workoutItemContainer">
+        <li className="indivWorkouts">
+          <strong>{workout.name}</strong>
+          <br></br>
+          <span>Description: {workout.description}</span>
+          <br></br>
+          <ul className="workoutExercises">Exercises: {allWorkoutExercises}</ul>
+          <br></br>
+          <Popup trigger={<button className="editButton">Edit</button>}>
+            <div className="popupTitle">Select Exercises to Add or Remove:</div>
+            <form className="workoutForm" onSubmit={handleAddExercises}>
+              <ul className="exerciseList">{allExercises}</ul>
+              <input type="submit" className="checkboxSubmit" />
+            </form>
+          </Popup>
+          <div class="divider"/>
+          <button className="workoutDeleteButton" onClick={() => handleDeleteWorkout(workout)}>Delete</button>
+        </li>
+    </div>
+  )
+}
 
 export default Workout;
