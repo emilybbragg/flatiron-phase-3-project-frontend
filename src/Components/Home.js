1df6cffcbea9509a6e2de82f7d12d021aac6cb42
Emily <emilybribragg@gmail.com>
2022-08-24 11:27:43 -0400

    Completed home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 function Home() {
-    return (
-        <>
-        <div className="greetingContainer">
-            <div className="greeting">{"Welcome to Don't Sweat It! Your personalized fitness tracker."}</div>
-            <div className="greetingLink"><a href="http://localhost:3000/workoutlist">Let's go!</a></div>
-        </div>
-        </>
-    );
+
+  return (
+    <>
+      <div className="greetingContainer">
+          <div className="greeting">{"Welcome to Don't Sweat It! Your personalized fitness tracker."}</div>
+          <div className="greetingLink"><a href="http://localhost:3000/workoutlist">Let's go!</a></div>
+      </div>
+      </>
+  );
 }
 
 export default Home;
