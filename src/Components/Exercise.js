4c2a3797c34255aff95a112a2747beea4cca2ac8
Emily <emilybribragg@gmail.com>
2022-08-24 11:26:27 -0400

    Handled exercise connection issue

--- src/Components/Exercise.js
+++ src/Components/Exercise.js
@@ -2,22 +2,19 @@ import React from "react";
 
 function Exercise( {exercise, handleExerciseDeleteClick} ) {
 
-    return (
-        <div className="exerciseItemContainer">
-        <li className="exerciseItems">
-            <strong>{exercise.name}</strong>
-            <br></br>
-            <span>Category: {exercise.category}</span>
-            <br></br>
-            <span>Description: {exercise.description}</span>
-            <br></br>
-            <button className="deleteButton gg-trash"
-              onClick={() => handleExerciseDeleteClick(exercise)}>
-            </button>
-        </li>
-        </div>
-      );
-  }
-
+  return (
+      <div className="exerciseItemContainer">
+          <li className="indivExercises">
+              <strong>{exercise?.name}</strong>
+              <br></br>
+              <span>Category: {exercise?.category?.name}</span>
+              <br></br>
+              <span>Description: {exercise?.description}</span>
+              <br></br>
+              <button className="deleteButton" onClick={() => handleExerciseDeleteClick(exercise)}>Delete</button>
+          </li>
+      </div>
+    );
+}
 
 export default Exercise;
