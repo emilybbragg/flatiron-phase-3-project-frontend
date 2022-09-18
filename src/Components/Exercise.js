354eb14ab40d62aa62fd022e5a884a07d78a3539
Emily <emilybribragg@gmail.com>
2022-09-18 17:50:39 -0400

    Reformatting code

--- src/Components/Exercise.js
+++ src/Components/Exercise.js
@@ -3,17 +3,17 @@ import React from "react";
 function Exercise( {exercise, handleExerciseDeleteClick} ) {
 
   return (
-      <div className="exerciseItemContainer">
-          <li className="indivExercises">
-              <strong>{exercise?.name}</strong>
-              <br></br>
-              <span>Category: {exercise?.category?.name}</span>
-              <br></br>
-              Description: {exercise?.description}
-              <br></br>
-              <button className="deleteButton" onClick={() => handleExerciseDeleteClick(exercise)}>Delete</button>
-          </li>
-      </div>
+    <div className="exerciseItemContainer">
+      <li className="indivExercises">
+        <strong>{exercise?.name}</strong>
+        <br></br>
+        <span>Category: {exercise?.category?.name}</span>
+        <br></br>
+        Description: {exercise?.description}
+        <br></br>
+        <button className="deleteButton" onClick={() => handleExerciseDeleteClick(exercise)}>Delete</button>
+      </li>
+    </div>
     );
 }
 
